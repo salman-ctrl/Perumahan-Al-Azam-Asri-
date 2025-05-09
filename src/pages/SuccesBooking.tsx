cb4e4b3e1ed392398529d27a989eb726c28ad861
salman-ctrl <zirmanvictory@gmail.com>
2025-05-09 23:51:09 +0700

    tambah tailwind

--- src/pages/SuccesBooking.tsx
+++ src/pages/SuccesBooking.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const SuccesBooking = () => {
     return (
-        <div>
-            halaman Sukses Buking
-        </div>
+        <>
+            <div>
+                <p className='text-blue-400 text-3xl ml-10 mt-10'>ini halaman sukses buking</p>
+            </div>
+        </>
     )
 }
-
-export default SuccesBooking
+export default SuccesBooking;

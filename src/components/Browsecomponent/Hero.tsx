9455144f3f9b9190ee1d913c12e29703132e4010
salman-ctrl <zirmanvictory@gmail.com>
2025-05-13 23:52:51 +0700

    tambah carousel

--- src/components/Browsecomponent/Hero.tsx
+++ src/components/Browsecomponent/Hero.tsx
@@ -346,11 +346,7 @@ const Hero = () => {
                         </div>
                         <div className="flex items-center gap-4">
                             <div className="flex items-center justify-center shrink-0 w-[70px] h-[70px] rounded-[23px] bg-white overflow-hidden">
-                                <img
-                                    src="src\assets\images\icons\home-trend-up.svg"
-                                    className="w-[34px] h-[34px]"
-                                    alt="icon"
-                                />
+
                             </div>
                             <div className="flex flex-col gap-[5px]">
                                 <p className="font-bold text-lg leading-[27px]">Sustain for Business</p>

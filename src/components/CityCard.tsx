2251adcfbd068b0bc27551c065c36c9ba99b0232
salman-ctrl <zirmanvictory@gmail.com>
2025-05-18 23:18:33 +0700

    menambahkan Office card

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -18,7 +18,12 @@ const CityCard = ({ city }: CityCardProps) => {
                     {/* Gradient overlay yang transparan */}
                     <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/70 z-10"></div>
                     {/* Konten teks di atas gambar */}
-
+                    <div className="relative flex flex-col justify-end w-full h-full p-5 gap-[2px] z-20">
+                        <h3 className="font-bold text-xl leading-[30px] text-white">
+                            {city.name}
+                        </h3>
+                        <p className="text-white">{city.officeSpaces_count} Offices</p>
+                    </div>
                 </div>
             </a>
         </div>

0ae6964c9dd6e2454f6b87c19a738c0e788f3ced
salman-ctrl <zirmanvictory@gmail.com>
2025-05-18 23:14:32 +0700

    perbaiki api

--- src/components/OfficeCard.tsx
+++ src/components/OfficeCard.tsx
@@ -17,7 +17,24 @@ const OfficeCard = ({ office }: OfficeCardProps) => {
                     <div className='px-3'>
                         <h2 className='line-clamp-2 px-3 font-bold text-[22px] leading-[36px] h-[72px] ' >Masayori Future Space</h2>
                         <div className='space-y-6'>
-
+                            <div className='flex justify-between px-3 items-center'>
+                                <p className='leading-[36px]'>Rp {office.price}</p>
+                                <div className='flex items-center gap-2'>
+                                    <p>Days {office.duration}</p>
+                                    <ClockIcon className='h-7 w-7 text-blue-500' />
+                                </div>
+                            </div>
+                            <hr className="border-[#F6F5FD]" />
+                            <div className='flex justify-between px-3 items-center'>
+                                <div className='flex items-center gap-2 '>
+                                    <MapPinIcon className='h-7 w-7 text-blue-500' />
+                                    <p>{office.city.name}</p>
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <p>4.3/3</p>
+                                    <StarIcon className='h-7 w-7 text-amber-300' />
+                                </div>
+                            </div>
                             <hr className="border-[#F6F5FD]" />
                             <div className='flex items-center justify-between px-3'>
                                 <div className='flex items-center gap-2'>
